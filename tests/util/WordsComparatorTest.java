64610f21888f0690e33c8910ae558e0427073595
Marek Stopka <keramakpots@gmail.com>
2018-06-27 21:59:48 +0200

    Change example Strings fields in test class

--- tests/util/WordsComparatorTest.java
+++ tests/util/WordsComparatorTest.java
@@ -6,28 +6,25 @@ import org.junit.jupiter.api.BeforeAll;
 
 public class WordsComparatorTest {
     private WordsComparator wordsComparator;
-    private String FIRST_EXAMPLE_WORD;
-    private String SECOND_EXAMPLE_WORD;
-    private String THIRD_EXAMPLE_WORD;
+    private final String FIRST_EXAMPLE_WORD  = "abcd";
+    private final String WORD_WITH_MORE_THEN_ONE_DIFFERENCE  = "bcda";
+    private final String LONGER_EXAMPLE_WORD = "bdcsda";
+    private final String WORD_WITH_ONE_DIFFERENCE = "abcc";
 
     @BeforeAll
     public void setUp() {
         wordsComparator = new WordsComparator();
-        FIRST_EXAMPLE_WORD = "abcd";
-        SECOND_EXAMPLE_WORD = "bcda";
-        THIRD_EXAMPLE_WORD = "bdcsda";
     }
 
     @Test
     public void testIfMethodToCompareWordsLengthReturnTrueIfTheyAreEqual() {
         Assert.assertTrue(wordsComparator.areWordsTheSameLength(FIRST_EXAMPLE_WORD,
-            SECOND_EXAMPLE_WORD));
+            WORD_WITH_MORE_THEN_ONE_DIFFERENCE));
     }
     @Test
     public void testIfMethodToCompareWordsLengthReturnFalseIfTheyAreNotEqual() {
         Assert.assertFalse(wordsComparator.areWordsTheSameLength(FIRST_EXAMPLE_WORD,
-            THIRD_EXAMPLE_WORD));
+            LONGER_EXAMPLE_WORD));
     }
-
 
 }
