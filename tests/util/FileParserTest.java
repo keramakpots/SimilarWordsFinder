77e48e6f0d2eb81f39cece792fc491d8a0d5c66c
Marek Stopka <keramakpots@gmail.com>
2018-06-27 21:50:31 +0200

    Add test if method loads empty string

--- tests/util/FileParserTest.java
+++ tests/util/FileParserTest.java
@@ -4,19 +4,29 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FileParserTest {
     private final String DICTIONARY_FILE = "testResources/test dictionary.txt";
     private FileParser fileParser;
 
+    @Before
+    public void setUp() {
+        fileParser = new FileParser();
+    }
+
     @Test
     public void testIfFileReaderLoadsEveryWordFromFile() throws IOException {
-        fileParser = new FileParser();
         List<String> testStrings = Arrays.asList("koń", "toń", "goń", "Słoń");
         List<String> loadedStrings = fileParser.getWordsFromFile(DICTIONARY_FILE);
         for (String string:testStrings) {
             Assert.assertTrue(loadedStrings.contains(string));
         }
+    }
+    @Test
+    public void testIfFileParserDoNotLoadsEmptyString() throws IOException {
+        List<String> loadedStrings = fileParser.getWordsFromFile(DICTIONARY_FILE);
+        Assert.assertFalse(loadedStrings.contains(""));
     }
 }
