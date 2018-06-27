2648e2c67429cd10e49a65a0debacdd26b084edd
Marek Stopka <keramakpots@gmail.com>
2018-06-27 20:25:49 +0200

    Add comparator class with method to compare words length

---
+++ tests/util/WordsComparator.java
@@ -0,0 +1,8 @@
+package util;
+
+public class WordsComparator {
+
+    public boolean areWordsTheSameLength(String firstComparableString, String secondComparableString) {
+        return firstComparableString.length() == secondComparableString.length();
+    }
+}
