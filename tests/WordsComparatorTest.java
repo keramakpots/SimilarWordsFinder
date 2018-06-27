0b9b5b2f57098c42a5ac31bf96e75455d7a3eb65
Marek Stopka <keramakpots@gmail.com>
2018-06-27 20:14:32 +0200

    Change class name

--- tests/WordsComparatorTest.java
+++ tests/WordsComparatorTest.java
@@ -7,6 +7,7 @@ public class WordsComparatorTest {
     private WordsComparator wordsComparator;
     private final String FIRST_EXAMPLE_WORD = "abcd";
     private final String SECOND_EXAMPLE_WORD = "bdca";
+    private final String THIRD_EXAMPLE_WORD = "bdcsda";
 
     @Before
     public void setUp() {
@@ -17,6 +18,11 @@ public class WordsComparatorTest {
     public void testIfMethodToCompareWordsLengthReturnTrueIfTheyAreEqual() {
         Assert.assertTrue(wordsComparator.areWordsTheSameLength(FIRST_EXAMPLE_WORD,
             SECOND_EXAMPLE_WORD));
+    }
+    @Test
+    public void testIfMethodToCompareWordsLengthReturnFalseIfTheyAreNotEqual() {
+        Assert.assertFalse(wordsComparator.areWordsTheSameLength(FIRST_EXAMPLE_WORD,
+            THIRD_EXAMPLE_WORD));
     }
 
 
