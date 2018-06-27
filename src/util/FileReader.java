2403e10f180d7023b89f87b33ee4dbf1b1faa73d
Marek Stopka <keramakpots@gmail.com>
2018-06-27 21:25:24 +0200

    Add implementation for loading list of words from a file

--- src/util/FileReader.java
+++ src/util/FileReader.java
@@ -1,10 +1,37 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
+    final static Charset ENCODING = StandardCharsets.UTF_8;
+    public List<String> getWordsFromFile(String pathToFile) throws IOException {
 
-    public List<String> getWordsFromFile(String pathToFile) {
-        return null;
-    }
+
+            List<String> list = new ArrayList<>();
+            Path path = Paths.get(pathToFile);
+
+            try (BufferedReader reader = Files.newBufferedReader(path, ENCODING)) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String line1 = line.trim().replaceAll("\uFEFF", "");
+                    list.add(line1);
+                }
+                reader.close();
+
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.get(i).length() <= 1) {
+                        list.remove(i);
+                    }
+                }
+            }
+            return list;
+        }
 }
