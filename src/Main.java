268ec96f3c4910e8c14a5dd248ffba49a4132367
Marek Stopka <keramakpots@gmail.com>
2018-06-28 22:02:43 +0200

    Add path to file

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,18 @@ public class Main {
     public static void main(String[] args) {
         final String START_WORD = "lead";
         final String FINAL_WORD = "goat";
+        final String PATH_TO_FILE = "resources/dictionary.txt";
         try {
             long startTime = System.currentTimeMillis();
             List<String> chainFromFirstWordToDesired = Arrays.asList(START_WORD, FINAL_WORD);
             List<String> dictionary = WordsComparator.getSameLengthWords(
-                FileParser.getWordsFromFile(args[1]), START_WORD.length());
+                FileParser.getWordsFromFile(PATH_TO_FILE), START_WORD.length());
 
             Chain chain = new Chain(dictionary, chainFromFirstWordToDesired);
             chain.print();
             chain.findShortestSuitableChain();
             long endTime = System.currentTimeMillis();
+            System.out.println("Founded in " + ((endTime - startTime) / 1000 % 60) + "miliseconds");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
