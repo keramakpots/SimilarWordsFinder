084087426f4fb1059383349662c8d1df68711773
Marek Stopka <keramakpots@gmail.com>
2018-06-28 21:04:48 +0200

    Add Chain fields

--- src/model/Chain.java
+++ src/model/Chain.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.util.List;
+
 public class Chain {
+
+    private List<String> dictionary;
+    private List<String> chainFromFirstWordToDesired;
+    private String firstWord;
+    private String desiredWord;
 
 }
