a34b744670ae643d84343f20a106b84d75bf69b9
Marek Stopka <keramakpots@gmail.com>
2018-06-28 21:54:05 +0200

    Implement main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import model.Chain;
+import util.FileParser;
+import util.WordsComparator;
+
 public class Main {
 
     public static void main(String[] args) {
+        final String START_WORD = "lead";
+        final String FINAL_WORD = "goat";
+        try {
+            long startTime = System.currentTimeMillis();
+            List<String> chainFromFirstWordToDesired = Arrays.asList(START_WORD, FINAL_WORD);
+            List<String> dictionary = WordsComparator.getSameLengthWords(
+                FileParser.getWordsFromFile(args[1]), START_WORD.length());
 
+            Chain chain = new Chain(dictionary, chainFromFirstWordToDesired);
+            chain.print();
+            chain.findShortestSuitableChain();
+            long endTime = System.currentTimeMillis();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
