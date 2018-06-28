83ffdd9aa1cc93b76c5c6fe88b4d96bb3db9ca8f
Marek Stopka <keramakpots@gmail.com>
2018-06-28 22:09:17 +0200

    Change test for static class

--- tests/util/FileParserTest.java
+++ tests/util/FileParserTest.java
@@ -4,29 +4,22 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class FileParserTest {
     private final String DICTIONARY_FILE = "testResources/test dictionary.txt";
-    private FileParser fileParser;
-
-    @Before
-    public void setUp() {
-        fileParser = new FileParser();
-    }
 
     @Test
     public void testIfFileReaderLoadsEveryWordFromFile() throws IOException {
         List<String> testStrings = Arrays.asList("koń", "toń", "goń", "Słoń");
-        List<String> loadedStrings = fileParser.getWordsFromFile(DICTIONARY_FILE);
+        List<String> loadedStrings = FileParser.getWordsFromFile(DICTIONARY_FILE);
         for (String string:testStrings) {
             Assert.assertTrue(loadedStrings.contains(string));
         }
     }
     @Test
     public void testIfFileParserDoNotLoadsEmptyString() throws IOException {
-        List<String> loadedStrings = fileParser.getWordsFromFile(DICTIONARY_FILE);
+        List<String> loadedStrings = FileParser.getWordsFromFile(DICTIONARY_FILE);
         Assert.assertFalse(loadedStrings.contains(""));
     }
 }
