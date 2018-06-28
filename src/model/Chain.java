5795811abef22255a34360a316d64ae886fbf3a6
Marek Stopka <keramakpots@gmail.com>
2018-06-28 21:33:16 +0200

    Add implemented method for finding words chains

--- src/model/Chain.java
+++ src/model/Chain.java
@@ -1,6 +1,11 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import util.WordsComparator;
 
 public class Chain {
 
@@ -47,5 +52,66 @@ public class Chain {
 
     public void setDesiredWord(String desiredWord) {
         this.desiredWord = desiredWord;
+    }
+
+    private void writeChildrens(Branch branch) {
+        List<String> currentDictionary = new ArrayList<>(dictionary);
+        currentDictionary.removeAll(branch.returnParentData());
+        List<Branch> children = new ArrayList<>();
+        String data = branch.getData();
+
+        for (String st : currentDictionary) {
+            if (WordsComparator.areWordsDifferentByOneWord(data, st)) {
+                children.add(new Branch(st, branch));
+            }
+        }
+        branch.setChildren(children);
+    }
+
+    private void buildBranch(Branch branch) {
+        writeChildrens(branch);
+        List<Branch> children = branch.getChildren();
+        if (children.size() > 0) {
+            for (Branch child : children) {
+                buildBranch(child);
+            }
+        }
+    }
+
+    private void findSuitableChains(Branch branch, Map<Integer, List<String>> map) {
+        List<Branch> children = branch.getChildren();
+        if (children.size() > 0) {
+            for (Branch child : children) {
+                if (child.getData().equals(desiredWord)) {
+                    int i = map.size();
+                    map.put(i, child.returnParentData());
+                } else {
+                    findSuitableChains(child, map);
+                }
+            }
+        }
+    }
+
+    public void print() {
+        firstWord.print();
+    }
+
+    public void findShortestSuitableChain() {
+        Map<Integer, List<String>> map = new LinkedHashMap<>();
+        findSuitableChains(firstWord, map);
+
+        int shortChainIndex = 0;
+        int dictionarySize = dictionary.size();
+        for (Map.Entry<Integer, List<String>> entry : map.entrySet()) {
+            if (entry.getValue().size() < dictionarySize) {
+                dictionarySize = entry.getValue().size();
+                shortChainIndex = entry.getKey();
+            }
+        }
+        Collections.reverse(map.get(shortChainIndex));
+        List<String> list = new ArrayList<>(map.get(shortChainIndex));
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
